92b2556b247736adf235da95527d06eaf70c60c3
Momen Fnnana <mommen@hemma.sa>
2022-10-08 10:13:42 +0300

    remove unnecessary code style

--- src/screens/Onboarding/styles.js
+++ src/screens/Onboarding/styles.js
@@ -1,28 +1,10 @@
-import { Dimensions } from 'react-native';
-import { ScaledSheet } from 'react-native-size-matters'
-import { Colors, Fonts } from '../../utils';
+import { ScaledSheet } from "react-native-size-matters";
+import { Colors, Fonts } from "../../utils";
 
 const styles = ScaledSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.white,
-  },
-  bullets: {
-    flexDirection: 'row',
-    alignSelf: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    bottom: 185,
-  },
-  primaryBullet: {
-    color: Colors.primary,
-    marginHorizontal: 2,
-    fontSize: 24,
-  },
-  grayBullet: {
-    color: Colors.gray,
-    marginHorizontal: 2,
-    fontSize: 16,
   },
   button: {
     backgroundColor: Colors.white,
@@ -31,14 +13,18 @@ const styles = ScaledSheet.create({
     width: 80,
     height: 40,
     borderRadius: 8,
-    alignSelf: 'flex-end',
+    alignSelf: "flex-end",
     bottom: 20,
     right: 20,
   },
   title: {
     color: Colors.primary,
     fontFamily: Fonts.SemiBold,
-  }
-})
+  },
+  dotsContentContainerStyle: {
+    justifyContent: "center",
+    flex: 1,
+  },
+});
 
 export default styles;
